39dd6a6a97f1d1b915d121b3d3eb54ae1befc6ff
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-05-26 15:33:01 -0500

    Rewrite spec; convert line endings to unix

--- spec/pdf_stamper_spec.rb
+++ spec/pdf_stamper_spec.rb
@@ -2,20 +2,17 @@ $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require 'pdf/stamper'
 
 describe PDF::Stamper do
-  before(:each) do
-    @pdf = PDF::Stamper.new(File.join(File.dirname(__FILE__), "test_template.pdf"))
-    @pdf.text :text_field01, "test"
-    @pdf.text :text_field02, "test2"
-    @pdf.image :button_field01, File.join(File.dirname(__FILE__), "logo.gif")
+  it 'should create PDF document' do
+    PDF::Stamper.new(File.join(File.dirname(__FILE__), 'test_template.pdf')).to_s.should_not be_nil
   end
 
-  it "should create PDF document" do
-    @pdf.to_s.should_not be_nil
-  end
-
-  it "should save PDF document" do
-    @pdf.save_as "test_output.pdf"
-    File.exist?("test_output.pdf").should be true
-    File.delete("test_output.pdf") # Comment this out to view the output
+  it 'should save PDF document' do
+    pdf = PDF::Stamper.new(File.join(File.dirname(__FILE__), 'test_template.pdf'))
+    pdf.text :text_field01, 'test'
+    pdf.text :text_field02, 'test2'
+    pdf.image :button_field01, File.join(File.dirname(__FILE__), 'logo.gif')
+    pdf.save_as 'test_output.pdf'
+    File.exist?('test_output.pdf').should be true
+    File.delete('test_output.pdf')
   end
 end
